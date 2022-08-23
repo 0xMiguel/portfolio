ce218af3cacd001346cd466b32b527a5fcf4d94b
morrocoyy <unahyp3@icloud.com>
2022-08-23 15:12:35 -0400

    feat(web): first part of the portfolio done

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Inter', ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          // Customize it on globals.css :root
+          dark: 'rgb(var(--tw-color-primary-dark) / <alpha-value>)',
+          medium: 'rgb(var(--tw-color-primary-medium) / <alpha-value>)',
+        },
+        "red-dark": "#CE3333",
+        "red-primary": "#D75B5B",
+        "red-light": "#F5D6D6",
+        "red-regular": "#EBADAD",
+        "dark": "#4933CE",
+        "medium-regular": "#B6ADEB",
+        "medium-dark": "#6C5BD7",
+        medium: "#9184E1",
+        light: "#DAD6F5",
+      },
+      keyframes: {
+        flicker: {
+          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
+            opacity: 0.99,
+            filter:
+              'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
+          },
+          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
+            opacity: 0.4,
+            filter: 'none',
+          },
+        },
+        shimmer: {
+          '0%': {
+            backgroundPosition: '-700px 0',
+          },
+          '100%': {
+            backgroundPosition: '700px 0',
+          },
+        },
+      },
+      animation: {
+        flicker: 'flicker 3s linear infinite',
+        shimmer: 'shimmer 1.3s linear infinite',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
