6e3e4028340b0a260fc18503d616fc0f6354e4eb
morrocoyy <unahyp3@icloud.com>
2023-04-06 15:28:12 -0400

    feat(motion): added motion and hrefs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import * as React from 'react';
 
 import Box from '@/components/Box';
+import MotionBox from '@/components/MotionBox';
 
 import Layout from '@/features/Layout';
 import Seo from '@/features/Seo';
@@ -22,16 +24,26 @@ export default function HomePage() {
           </div>
         </div>
         <div className='mt-10 flex flex-col gap-y-8 md:gap-x-8 lg:flex-row'>
-          <Box className='flex w-full cursor-pointer justify-center py-20 lg:w-3/12'>
-            <div>
-              <img src='/images/twitter_logo_blue.svg' alt='twitter' />
-            </div>
-          </Box>
-          <Box className='flex w-full cursor-pointer justify-center py-20 lg:w-3/12'>
-            <div>
-              <img src='/images/github_logo.svg' alt='github' />
-            </div>
-          </Box>
+          <Link href='https://twitter.com/miguelodev' passHref>
+            <MotionBox
+              whileHover={{ y: -10 }}
+              className='flex w-full cursor-pointer justify-center py-20 lg:w-3/12'
+            >
+              <div>
+                <img src='/images/twitter_logo_blue.svg' alt='twitter' />
+              </div>
+            </MotionBox>
+          </Link>
+          <Link href='https://github.com/0xMiguel' passHref>
+            <MotionBox
+              whileHover={{ y: -10 }}
+              className='flex w-full cursor-pointer justify-center py-20 lg:w-3/12'
+            >
+              <div>
+                <img src='/images/github_logo.svg' alt='github' />
+              </div>
+            </MotionBox>
+          </Link>
           <Box className='flex-1 flex-shrink-0  border-2 border-purple-500 bg-purple-gradient'>
             <div className='justify-left mt-4 flex flex-col items-center justify-center lg:flex-row'>
               <img
@@ -73,16 +85,24 @@ export default function HomePage() {
             </div>
           </Box>
           <div className='flex cursor-pointer flex-col items-center justify-center gap-y-4'>
-            <Box className='flex w-full justify-center py-10 px-10'>
-              <div>
-                <img src='/images/calendar.svg' alt='my calendar' />
-              </div>
-            </Box>
-            <Box className='flex w-full cursor-pointer justify-center py-10 px-10'>
+            <Link href='https://cal.com/0xmiguel' passHref>
+              <MotionBox
+                whileHover={{ y: -5 }}
+                className='flex w-full justify-center py-10 px-10'
+              >
+                <div>
+                  <img src='/images/calendar.svg' alt='my calendar' />
+                </div>
+              </MotionBox>
+            </Link>
+            <MotionBox
+              whileHover={{ y: -5 }}
+              className='flex w-full cursor-pointer justify-center py-10 px-10'
+            >
               <div>
                 <img src='/images/discord_logo.svg' alt='my calendar' />
               </div>
-            </Box>
+            </MotionBox>
           </div>
           <Box className='flex-shrink-0 items-center justify-center'>
             <div className='flex flex-row justify-center p-4'>
